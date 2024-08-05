d989e957d0b006ea84f87b58ec20e9d36d5593ff
TitoGblin <140563527+titoCo12@users.noreply.github.com>
2024-08-05 14:17:50 -0300

    Update Triggers-Funciones.sql

--- Esquema-Scheme/Triggers-Funciones.sql
+++ Esquema-Scheme/Triggers-Funciones.sql
@@ -1,1 +1,20 @@
 
+CREATE OR REPLACE FUNCTION gasto_agregado() 
+RETURNS TRIGGER 
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+	UPDATE total 
+	SET gastoTotal = gastoTotal + NEW.gasto, 
+	cantUsable = cantUsable - NEW.gasto,
+	gananciavsgastos = gananciavsgastos - NEW.gasto;
+	RETURN NULL;
+	END;
+$$;
+
+
+CREATE TRIGGER gasto_agregado_tg
+AFTER INSERT 
+ON gastosMP
+FOR EACH ROW
+EXECUTE PROCEDURE gasto_agregado();
